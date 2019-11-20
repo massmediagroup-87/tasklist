ddd5871d2ae5aa07cf48dd42fcaf5ca17a191a87
Shamanskyi Bogdan <mmg.shamanskyi.b@gmail.com>
2019-11-20 15:22:16 +0200

    Task block for TaskList component

---
+++ src/components/Task.vue
@@ -0,0 +1,31 @@
+<template>
+    <li v-if="!status ? !checked : true" class="list-group-item alert alert-warning alert-dismissible fade show mb-2" role="alert">
+        {{ todo.text }}
+        <label class="close" data-dismiss="alert" aria-label="Close">
+            <input v-model="checked" type="checkbox" aria-label="Checkbox for following text input">
+        </label>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "Task",
+        props: {
+            'todo': {
+                type: Object
+            },
+            'status' : {
+                type: Boolean
+            }
+        },
+        data: function () {
+            return {
+                checked: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
