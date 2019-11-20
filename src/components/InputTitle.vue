717db8a1ab9b9b977f7f63ec48c8ed4f3393a573
Shamanskyi Bogdan <mmg.shamanskyi.b@gmail.com>
2019-11-20 15:20:45 +0200

    Input group for add tasks

---
+++ src/components/InputTitle.vue
@@ -0,0 +1,29 @@
+<template :text="text">
+    <div class="input-group mb-3">
+        <input aria-describedby="basic-addon2" class="form-control" placeholder="Add task" type="text" v-model="text">
+        <button @click="addTask" class="btn btn-primary">Add</button>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "InputTitle",
+        components: {},
+        data: () => ({
+            text: ''
+        }),
+        methods: {
+            addTask(e) {
+                this.$parent.$emit('add-task', this.text)
+
+                e.target.parentNode.firstChild.value = '';
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
