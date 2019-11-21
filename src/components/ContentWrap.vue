11ab860fc51797b81661911702351df97230c15f
Shamanskyi Bogdan <mmg.shamanskyi.b@gmail.com>
2019-11-21 15:41:38 +0200

    Moved the basic logic from TaskList and update methods for data

--- src/components/ContentWrap.vue
+++ src/components/ContentWrap.vue
@@ -2,10 +2,10 @@
     <div>
         <input-title/>
         <div class="custom-control custom-checkbox mr-sm-2 mb-3">
-            <input v-model="checked" type="checkbox" class="custom-control-input" id="customControlAutosizing">
+            <input class="custom-control-input" id="customControlAutosizing" type="checkbox" v-model="checked">
             <label class="custom-control-label" for="customControlAutosizing">Show all tasks</label>
         </div>
-        <task-list :status="checked"/>
+        <task-list :check-all="checked" :tasks="tasks"/>
     </div>
 </template>
 
@@ -19,8 +19,65 @@
             InputTitle,
             TaskList
         },
-        data: ()=> ({
-            checked: false
-        })
+        data: () => ({
+            checked: false,
+            tasks: [],
+            text: ''
+        }),
+        updated() {
+            this.setStorage('checked', this.checked);
+
+            this.sortTasks(this.tasks);
+        },
+        created() {
+            this.addTaskStorage();
+
+            if (this.getStorage('checked')) this.checked = this.getStorage('checked');
+
+            this.$on('add-task', this.addTask);
+        },
+        methods: {
+            getStorage(key) {
+                return JSON.parse(localStorage.getItem(key));
+            },
+            setStorage(key, value) {
+                localStorage[key] = JSON.stringify(value);
+            },
+            addTask(text) {
+                if (text.length > 2) {
+                    this.tasks.unshift({
+                        id: new Date().getTime(),
+                        text: text,
+                        active: false
+                    })
+                }
+
+                this.setStorage('tasks', this.tasks);
+            },
+            addTaskStorage() {
+                let tasks = this.getStorage('tasks');
+
+                if (tasks !== null) {
+                    tasks.forEach((task) => {
+                        this.tasks.push(task)
+                    })
+                }
+            },
+            sortTasks(arr) {
+                for (let i = 0, endI = arr.length - 1; i < endI; i++) {
+                    let wasSwap = false;
+
+                    for (let j = 0, endJ = endI - i; j < endJ; j++) {
+                        if (arr[j].active < arr[j + 1].active) {
+                            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                            wasSwap = true;
+                        }
+                    }
+
+                    if (!wasSwap) break;
+                }
+                return arr;
+            }
+        }
     }
 </script>
