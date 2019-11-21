2763ae2dc15ca6dae71c3977d79da0ee20a94ae1
Shamanskyi Bogdan <mmg.shamanskyi.b@gmail.com>
2019-11-21 15:49:52 +0200

    Working with data and added method for delete task

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,31 +1,50 @@
 <template>
-    <li v-if="!status ? !checked : true" class="list-group-item alert alert-warning alert-dismissible fade show mb-2" role="alert">
+
+    <li class="list-group-item alert alert-warning alert-dismissible fade show mb-2" role="alert"
+        v-if="checkAll ? true : !checked">
         {{ todo.text }}
-        <label class="close" data-dismiss="alert" aria-label="Close">
-            <input v-model="checked" type="checkbox" aria-label="Checkbox for following text input">
+        <label aria-label="Close" class="close mr-4" data-dismiss="alert">
+            <input aria-label="Checkbox for following text input" type="checkbox" v-model="checked">
+            <span aria-hidden="true" class="mr-4"></span>
         </label>
+
+        <button @click="destroyTask" aria-label="Close" class="close" data-dismiss="alert" type="button" v-if="checked">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </li>
+
 </template>
 
 <script>
+
     export default {
         name: "Task",
         props: {
-            'todo': {
-                type: Object
-            },
-            'status' : {
-                type: Boolean
-            }
+            'todo': Object,
+            'checkAll': Boolean
         },
-        data: function () {
-            return {
-                checked: false
+        data: () => ({
+            checked: false
+        }),
+        created() {
+            if (this.todo.active) this.checked = true;
+        },
+        updated() {
+            this.todo.active = this.checked;
+            this.setStorage('tasks', this.$parent.tasks);
+        },
+        methods: {
+            getStorage(key) {
+                return JSON.parse(localStorage.getItem(key));
+            },
+            setStorage(key, value) {
+                localStorage[key] = JSON.stringify(value);
+            },
+            destroyTask() {
+                let index = this.$parent.tasks.indexOf(this.todo);
+                this.$parent.tasks.splice(index, 1);
+                this.setStorage('tasks', this.$parent.tasks);
             }
         }
     }
 </script>
-
-<style scoped>
-
-</style>
